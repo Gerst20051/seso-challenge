f3d2a60f7140e6b319c13cfadee5c7ae563c1bfb
Andrew Gerst <gerst20051@gmail.com>
2024-06-28 19:05:07 -0700

    implement solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,7 +2,16 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+module.exports = async (logSources, printer) => {
+  while (!logSources.every(logSource => logSource.drained)) {
+    logSources.sort((a, b) => a.last.date - b.last.date);
+    const oldestLog = logSources.find(logSource => !logSource.drained)
+    printer.print(oldestLog.last);
+    await oldestLog.popAsync();
+  }
+
+  printer.done();
+
   return new Promise((resolve, reject) => {
     resolve(console.log("Async sort complete."));
   });
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,14 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  while (!logSources.every(logSource => logSource.drained)) {
+    logSources.sort((a, b) => a.last.date - b.last.date);
+    const oldestLog = logSources.find(logSource => !logSource.drained)
+    printer.print(oldestLog.last);
+    oldestLog.pop();
+  }
+
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
